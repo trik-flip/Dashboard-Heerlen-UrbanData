255740214b23b77d32c95c313611cdc25c6de9bd
Philip Bollen <flip.philip@hotmail.com>
2021-06-18 15:38:02 +0200

    .

--- app.py
+++ app.py
@@ -132,7 +132,4 @@ def update_graph(color_filter, x_label, y_label):
 
 
 if __name__ == "__main__":
-    app.run_server(debug=False,
-                      port=44277,
-                   #    host="peaceful-wave-95791.herokuapp.com"
-                   )
+    app.run_server(debug=True)
